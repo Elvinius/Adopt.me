8909a97f55f9f0b14c51e80bba3951eb9dd4fd32
Elvinius <elwinabbasli@gmail.com>
2020-01-19 06:55:13 +0200

    Add npm packages, scripts for ESLint and prettier

--- src/app.js
+++ src/app.js
@@ -1,32 +1,25 @@
-const Pet = ({name, animal, breed}) => {
-	return React.createElement(
-		"div",
-		{},
-		[
-		React.createElement("h2", {}, name),
-		React.createElement("h2", {}, animal),
-		React.createElement("h2", {}, breed)
-		]);
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed)
+  ]);
 };
 
 const App = () => {
-	return React.createElement("div",{},
-				[
-				React.createElement("h1",{}, "Adopt Me!"),
-				React.createElement(Pet, {
-					name:"Sharik",
-					animal:"Dog", 
-					breed:"German shepherd"
-				}),
-				React.createElement(Pet, {
-					name:"Mauritius",
-					animal:"Cat", 
-					breed:"Angora"
-				})
-				]);		
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Sharik",
+      animal: "Dog",
+      breed: "German shepherd"
+    }),
+    React.createElement(Pet, {
+      name: "Mauritius",
+      animal: "Cat",
+      breed: "Angora"
+    })
+  ]);
 };
 
-ReactDOM.render(React.createElement(App),
-	document.querySelector("#root")
-		);
-
+ReactDOM.render(React.createElement(App), document.querySelector("#root"));
