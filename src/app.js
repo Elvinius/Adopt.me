9d27cb5819606c55b64d7344e721fc2b22f30ffd
Elvinius <elwinabbasli@gmail.com>
2020-01-21 22:52:19 +0200

    Add hooks and replace the old structure with hooks

--- src/app.js
+++ src/app.js
@@ -1,15 +1,13 @@
 import React from "react";
 import {render} from "react-dom"; //we use { render } to extract exactly render method from react-dom package
-import Pet from "./Pet";
+import SearchParams from "./SearchParams"
 
 
 const App = () => {
   return( 
   <div>
     <h1>Adopt Me!</h1>
-    <Pet name="Luna" animal="Dog" breed="Havanese" />
-    <Pet name="Messy" animal="Cat" breed="Angora" />
-    <Pet name="Schwartz" animal="Dog" breed="Doberman" />
+    <SearchParams />
   </div>);
 };
 
